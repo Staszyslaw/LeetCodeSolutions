d39760572636eac144a822b4cc2be4ebb58838c3
Stanisław Czajka <40602953+Staszyslaw@users.noreply.github.com>
2022-09-01 21:42:09 +0200

    Time: 104 ms (43.80%), Space: 43.3 MB (73.03%) - LeetHub

--- 27-remove-element/27-remove-element.ts
+++ 27-remove-element/27-remove-element.ts
@@ -1,11 +1,10 @@
 function removeElement(nums: number[], val: number): number {
-    for(let i=0;i<nums.length;i++) {
-        if(nums[i] === val) {
-            for(let j=i;j<nums.length;j++) {
-                nums[j] = nums[j+1];
-            }
-            i--;
+    let i = 0;
+    for(let j=0;j<nums.length;j++) {
+        if(nums[j] !== val) {
+            nums[i] = nums[j]
+            i++;
         }
     }
-    return nums.filter(x => x !== undefined).length;
+    return i;
 };
