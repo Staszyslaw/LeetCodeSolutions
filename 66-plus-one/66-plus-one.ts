583a9d3af961092ed186b72bd73b0972ec5486bd
Stanisław Czajka <40602953+Staszyslaw@users.noreply.github.com>
2022-09-02 10:09:41 +0200

    Time: 82 ms (76.51%), Space: 43.1 MB (58.53%) - LeetHub

---
+++ 66-plus-one/66-plus-one.ts
@@ -0,0 +1,16 @@
+function plusOne(digits: number[]): number[] {
+    digits[digits.length - 1]++;
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] === 10) {
+            digits[i] = 0;
+            if (i === 0) {
+                digits.unshift(1);
+            } else {
+                digits[i - 1]++;
+            }
+        } else {
+            break;
+        }
+    }
+    return digits;
+};
