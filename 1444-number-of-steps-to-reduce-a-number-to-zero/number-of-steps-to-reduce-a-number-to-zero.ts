f2b6788d3449e90de79edd2557883fa1597cad80
Stanisław Czajka <40602953+Staszyslaw@users.noreply.github.com>
2024-10-01 10:26:35 +0200

    Time: 67 ms (12.98%) | Memory: 51.8 MB (5.96%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.ts
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.ts
@@ -2,7 +2,7 @@ function numberOfSteps(num: number): number {
     let steps = 0;
 
     while (num > 0) {
-        if (num % 2 == 0) {
+        if (!(num % 2)) {
             num = num >> 1;
         } else {
             num--;
